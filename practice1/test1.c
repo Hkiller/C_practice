e7c6267e84ce3e9620b1068a06c1328ee93be2ee
hkiller <hkiller520@gmail.com>
2017-12-08 19:37:54 +0800

    update

---
+++ practice1/test1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+const int w = 20;
+const int h = 20;
+
+int f(int x, int y){
+	return (x - 10) * (x - 10) + (y - 10) * (y - 10) <= 8 * 8;
+}
+
+int main(){
+	int x,y;
+	for(y = 0; y < h; y++){
+		for(x = 0; x < w; x++){
+			printf(f(x, y)? "*" : " ");
+		}
+		puts("");
+	}
+}
